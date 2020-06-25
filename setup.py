330e4586ed429002f77ab730ecf46046eeb824f5
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-06-25 16:51:49 -0400

    Upped the version and include fuzzdb files in the package

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
+import os
 from setuptools import setup
 
 setup(name='protofuzz',
-      version='0.1',
+      version='0.2',
       description='Google protobuf message generator',
-      url='http://github.com/trailofbits/protofuzz',
-      author='Yan Ivnitskiy',
-      author_email='yan@trailofbits.com',
+      long_description=""""ProtoFuzz is a generic fuzzer for Google’s Protocol Buffers format.
+Instead of defining a new fuzzer generator for custom binary formats, protofuzz automatically creates a fuzzer based on
+the same format definition that programs use. ProtoFuzz is implemented as a stand-alone Python3 program.""",
+      url='https://github.com/trailofbits/protofuzz',
+      author='Trail of Bits',
       license='MIT',
       packages=['protofuzz'],
       install_requires=['protobuf>=2.6.0'],
-      include_package_data=True,
       test_suite='nose.collector',
       tests_require=['nose'],
-      zip_safe=False)
+      zip_safe=False,
+      package_data={'protofuzz': [os.path.join('fuzzdb', '**', '*')]},
+      include_package_data=True,
+)
