4715166dd492abc2b438b0a2edfe8c66b101b8a5
Spellchaser <klei22@bloomberg.net>
2021-08-18 15:50:57 -0400

    tests for import of generated code
    excludes ModuleNotFoundError:

--- protofuzz/tests/test_pbimport.py
+++ protofuzz/tests/test_pbimport.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import stat
 import shutil
 import tempfile
 import unittest
 
-from protofuzz import pbimport
+from protofuzz import pbimport, _compile_proto
 
 
 class TestPbimport(unittest.TestCase):
@@ -39,6 +40,50 @@ class TestPbimport(unittest.TestCase):
 
         module = pbimport.from_string(contents)
         self.assertTrue(hasattr(module, name))
+
+    def test_from_generated(self):
+        """
+        Get a protobuf module from generated file
+        """
+        name = 'Msg'
+        contents = 'message {} {{ required int32 var = 1; }}\n'.format(name)
+
+        filename = os.path.join(self.tempdir, "test.proto")
+        with open(filename, 'w') as f:
+            f.write(contents)
+    
+        dest = self.tempdir
+        full_path = os.path.abspath(proto_file)
+        _compile_proto(full_path, dest)
+
+        filename = os.path.split(full_path)[-1]
+        name = re.search(r'^(.*)\.proto$', filename).group(1)
+        target = os.path.join(dest, name+'_pb2.py')
+
+        module = pbimport.from_file(target)
+        self.assertTrue(hasattr(module, name))
+
+    def test_failing_import(self):
+        """
+        Test the failing of malformed generated protobuf file
+        """
+        contents = 'malformed generated code'
+
+        filename = os.path.join(self.tempdir, "test_pb2.py")
+        with open(filename, 'w') as f:
+            f.write(contents)
+    
+        with self.assertRaises(AttributeError):
+            module = pbimport.from_file(filename)
+
+    def test_failing_import_not_found(self):
+        """
+        Get a protobuf module from generated file
+        """
+        filename = os.path.join(self.tempdir, "test_pb2.py")
+    
+        with self.assertRaises(FileNotFoundError):
+            module = pbimport.from_file(filename)
 
     def test_generate_and_import(self):
         """
